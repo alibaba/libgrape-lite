bf4dde1f58aaf4abd277f1e685243fe18f674d3b
有理 <youli.lx@alibaba-inc.com>
2022-05-13 14:44:46 +0800

    update compile command

--- run/compile-flash.sh
+++ run/compile-flash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-mpic++ -std=c++14 -O3  ../src/flash.cpp -o flash
+#mpic++ -std=c++14 -O3  ../src/flash.cpp -o flash
 
-#mpic++ -std=c++14 -O3 -fopenmp ../src/flash.cpp -o flash
+mpic++ -std=c++14 -O3 -fopenmp ../src/flash.cpp -o flash
--- run/compile.sh
+++ run/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mpic++ -std=c++14 -O3 ../src/apps/$1.cpp -o $1
+#mpic++ -std=c++14 -O3 ../src/apps/$1.cpp -o $1
 
-#mpic++ -std=c++14 -O3 -fopenmp ../src/alg/$1.cpp -o $1
+mpic++ -std=c++14 -O3 -fopenmp ../src/alg/$1.cpp -o $1
