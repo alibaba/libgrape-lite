90ef7b1c8e824448484f281a5b90c158a62b54fb
Tao He <linzhu.ht@alibaba-inc.com>
2023-05-30 10:12:16 +0800

    Leave a find_package_message and fixes the VERSION in cmake modules
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- libgrapelite-config.in.cmake
+++ libgrapelite-config.in.cmake
@@ -12,3 +12,9 @@ include("${CMAKE_CURRENT_LIST_DIR}/libgrapelite-targets.cmake")
 set(LIBGRAPELITE_LIBRARIES grape-lite)
 set(LIBGRAPELITE_INCLUDE_DIR "${LIBGRAPELITE_HOME}/include")
 set(LIBGRAPELITE_INCLUDE_DIRS "${LIBGRAPELITE_INCLUDE_DIR}")
+
+include(FindPackageMessage)
+find_package_message(libgrapelite
+    "Found libgrapelite: ${CMAKE_CURRENT_LIST_FILE} (found version \"@LIBGRAPELITE_VERSION@\")"
+    "libgrapelite version: @LIBGRAPELITE_VERSION@\nlibgrapelite libraries: ${LIBGRAPELITE_LIBRARIES}, include directories: ${LIBGRAPELITE_INCLUDE_DIRS}"
+)
